0025241720a0ad1e4528c7d0fbd2f17ff47067aa
kayaneeeee <kayanem70@gmail.com>
2022-11-25 10:18:32 -0300

    versao 2 - placar atualizado e data

--- aula1.js
+++ aula1.js
@@ -1,7 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res){
     
-    res.write('Braasil 4 x 0 servia \n');
+    res.writeHead(200, {'Content-Type':'text/html'}); //para os comandos html funcionar
+    res.write('<h2>Braasil 2 x 0 servia<h2><br>');
+    
+    res.write(Date().substring(16,24)+'<br>'); //substring para filtrar uma parte dos dados da data
     res.end('bom dia');
 
 }).listen(3000);
