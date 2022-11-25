aaef40e956686856ba7d30b068d6a3339e7c9d5e
kayaneeeee <kayanem70@gmail.com>
2022-11-25 10:48:05 -0300

    versao 3 - add blibioteca data e dia da semana

--- aula1.js
+++ aula1.js
@@ -1,11 +1,12 @@
 var http = require('http');
+var dia = require('./bliblioteca'); //para importar, ligar um arquivo no outro 
 http.createServer(function (req, res){
-    
     res.writeHead(200, {'Content-Type':'text/html'}); //para os comandos html funcionar
     res.write('<h2>Braasil 2 x 0 servia<h2><br>');
-    
-    res.write(Date().substring(16,24)+'<br>'); //substring para filtrar uma parte dos dados da data
+    res.write( '<p style= color:blue>'+Date().substring(16,24)+'<br>'); //substring para filtrar uma parte dos dados da data
+    res.write(dia.diaDaSemana()+'</p><br>')    //chamar a variavel
     res.end('bom dia');
+
 
 }).listen(3000);
 
